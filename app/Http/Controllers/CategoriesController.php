eec92bc4b7ee27aabadb207411cfd33649e76f34
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2021-07-27 15:09:33 +0600

    update

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Categories;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
@@ -21,15 +22,18 @@ class CategoriesController extends Controller
         $categoryName = 'categoryName';
         $slug = 'slug';
         $request->validate(
-            ['categoryName' => 'required|regex:/^[a-zA-Z ]+$/|min:3|max:10|unique:categories'],
+            ['categoryName' => 'required|regex:/^[a-zA-Z ]+$/|min:3|max:10'], // {WITH unique:categories validate}
             ['categoryName.regex' => 'Please Type validate Category Name']
 
         );
+        $fileCheck = Categories::where($slug, Str::slug($request->$categoryName))->exists();
         $category = new Categories;
         $category->$categoryName = $request->$categoryName;
-        $category->$slug = Str::slug($request->$categoryName);
+        $fileCheck === true ? $category->$slug = Str::slug($request->$categoryName) . '-' . Str::random(8) : $category->$slug = Str::slug($request->$categoryName);
         $category->save();
         return redirect(route('categoriesAdd'));
+
+
 //        return back();
     }
 }
--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -10,4 +10,5 @@ class Categories extends Model
     use HasFactory;
 
 
+
 }
