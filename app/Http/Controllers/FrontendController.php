72ccb69333017ee0e63345a793eb7fdd49f942f8
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2021-10-16 21:15:54 +0600

    update

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Categories;
 use App\Models\Products;
 use App\Models\Products_thumbnail;
+use Illuminate\Support\Facades\View;
 
 class FrontendController extends Controller
 {
     public function index()
     {
         $products = Products::latest()->with(['category', 'subcategory'])->get();
-        return view("frontend.home", ["products" => $products]);
+        $category = Categories::with("products")->get();
+        return view("frontend.home", ["products" => $products,"categories" => $category]);
     }
 
     public function singleProduct($id)
@@ -26,7 +28,7 @@ class FrontendController extends Controller
     public function shop()
     {
         $products = Products::latest()->with(['category', 'subcategory'])->get();
-        $category = Categories::with('products')->get();
-        return view("frontend.shop", ["productData" => $products, 'categoryData' => $category]);
+        $category = Categories::with('products')->orderBy('categoryName')->get();
+        return view::make("frontend.shop", ["productData" => $products, 'categoryData' => $category]);
     }
 }
