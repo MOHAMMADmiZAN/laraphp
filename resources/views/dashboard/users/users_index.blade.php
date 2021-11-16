4215be43025d2a4f954672081e852830d33e4772
mohammadmiZAN <takbir.jcd@gmail.com>
2021-11-16 20:34:34 +0600

    request fix

---
+++ resources/views/dashboard/users/users_index.blade.php
@@ -0,0 +1,36 @@
+@extends('dashboard.master')
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="card bg-gray-500 mt-3">
+                        <div class="card-header">Users Details</div>
+                        <div class="card-body">
+                            <table class="table table-striped text-center table-bordered">
+                                <thead>
+                                <tr>
+                                    <th>User Name</th>
+                                    <th>User Email</th>
+                                    <th>User Role</th>
+                                    <th>User Photo</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                   <td></td>
+                                   <td></td>
+                                   <td></td>
+                                   <td></td>
+                                   <td></td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
