0dad29b4130c02f890d587671d9944a1f387788b
mohammadmiZAN <takbir.jcd@gmail.com>
2021-11-16 20:25:03 +0600

    request fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use Hash;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -25,8 +26,8 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'email_verified_at' => now('Asia/Dhaka'),
+            'password' => Hash::make('01307997323'), // password
             'remember_token' => Str::random(10),
         ];
     }
