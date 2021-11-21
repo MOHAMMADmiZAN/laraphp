c31e05c9c3cabcdab50893e1f4135ccc150a4111
mohammadmiZAN <takbir.jcd@gmail.com>
2021-11-21 13:44:11 +0600

    user fixed

--- resources/views/dashboard/users/users_index.blade.php
+++ resources/views/dashboard/users/users_index.blade.php
@@ -59,70 +59,81 @@
         let raw = document.getElementById('raw')
         let edit = document.getElementsByClassName('us-edit');
         let del = document.getElementsByClassName('us_del')
+        let auth_role = "{{Auth()->user()->role}}"
         // update loop
         for (let i = 0; i < edit.length; i++) {
+
             edit[i].addEventListener('click', function (e) {
-                e.preventDefault()
-                let id = this.getAttribute('data-id');
-                axios.get(`{{url('/user-edit')}}/${id}`).then(function (r) {
-                    raw.innerHTML = r.data[0]
-                    let btn = document.createElement('button')
-                    btn.classList.add('btn', 'btn-info', 'us_update')
-                    btn.innerText = 'Update';
-                    btn.setAttribute('id', r.data[1].id)
-                    let draw = raw.lastElementChild.children[1].children[1]
-                    draw.after(btn)
-                    let role = document.querySelector('#us_role')
-                    role.value = r.data[1].role
-                    let role_value = role.getAttribute('value')
-                    role.addEventListener('keyup', function (e) {
-                        role_value = e.target.value
+                if (auth_role !== 'user') {
+                    e.preventDefault()
+                    let id = this.getAttribute('data-id');
+                    axios.get(`{{url('/user-edit')}}/${id}`).then(function (r) {
+                        raw.innerHTML = r.data[0]
+                        let btn = document.createElement('button')
+                        btn.classList.add('btn', 'btn-info', 'us_update')
+                        btn.innerText = 'Update';
+                        btn.setAttribute('id', r.data[1].id)
+                        let draw = raw.lastElementChild.children[1].children[1]
+                        draw.after(btn)
+                        let role = document.querySelector('#us_role')
+                        role.value = r.data[1].role
+                        let role_value = role.getAttribute('value')
+                        role.addEventListener('keyup', function (e) {
+                            role_value = e.target.value
 
-                    })
-                    let up_url = "{{route('user-edit-response')}}"
-                    let role_th_id = '#role' + r.data[1].id
-                    btn.addEventListener('click', function (e) {
-                        axios.put(up_url, {id: r.data[1].id, role: role_value,}).then(function (r) {
-                            if (r.status === 200) {
-                                let role_th = document.querySelector(role_th_id)
-                                role_th.innerHTML = r.data.role
-                                setTimeout(function () {
-                                    raw.innerHTML = ''
-                                }, 500)
-                            }
-                        }).catch(function (e) {
-                            console.log(e)
                         })
+                        let up_url = "{{route('user-edit-response')}}"
+                        let role_th_id = '#role' + r.data[1].id
+                        btn.addEventListener('click', function (e) {
+                            axios.put(up_url, {id: r.data[1].id, role: role_value,}).then(function (r) {
+                                if (r.status === 200) {
+                                    let role_th = document.querySelector(role_th_id)
+                                    role_th.innerHTML = r.data.role
+                                    setTimeout(function () {
+                                        raw.innerHTML = ''
+                                    }, 500)
+                                }
+                            }).catch(function (e) {
+                                console.log(e)
+                            })
 
+                        })
+                    }).catch(function (e) {
+                        console.log(e)
                     })
-                }).catch(function (e) {
-                    console.log(e)
-                })
+                } else {
+                    alert('You Are Not Authenticated')
+                }
             })
+
         }
         // delete loop
         for (let i = 0; i < del.length; i++) {
             del[i].addEventListener('click', function (e) {
-                e.preventDefault()
-                let id = this.getAttribute('data-id');
-                let del_th_id = `del_tbody${id}`
-                let del_th = document.getElementById(del_th_id)
-                let del_url = `{{url('/user-delete')}}/${id}`
-                axios.delete(del_url).then(function (r) {
-                    if (r.status === 200) {
-                        if (r.data === 'Not Dumped') {
-                            alert('This User Is a Super Admin')
-                        } else {
-                            del_th.remove()
+                if (auth_role !== 'user') {
+                    e.preventDefault()
+                    let id = this.getAttribute('data-id');
+                    let del_th_id = `del_tbody${id}`
+                    let del_th = document.getElementById(del_th_id)
+                    let del_url = `{{url('/user-delete')}}/${id}`
+                    axios.delete(del_url).then(function (r) {
+                        if (r.status === 200) {
+                            if (r.data === 'Not Dumped') {
+                                alert('This User Is a Super Admin')
+                            } else {
+                                del_th.remove()
+                            }
                         }
-                    }
-                    if (document.querySelector('tbody').childElementCount < 1) {
-                        window.location.reload()
-                    }
+                        if (document.querySelector('tbody').childElementCount < 1) {
+                            window.location.reload()
+                        }
 
-                }).catch(function (e) {
-                    console.log(e)
-                })
+                    }).catch(function (e) {
+                        console.log(e)
+                    })
+                } else {
+                    alert("You are not allowed to delete")
+                }
             })
         }
     </script>
